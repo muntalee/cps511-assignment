2f923ac2aa7f86b28757f98244b87be8196f8f17
Munta Islam <islammuntasirul@gmail.com>
2025-09-29 14:40:51 -0400

    fix up freeglut

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,55 @@
-#include <GLFW/glfw3.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <glm/glm.hpp>
+#include <iostream>
 
-int main(void)
+// simple triangle
+void display()
 {
-    GLFWwindow* window;
+    glClear(GL_COLOR_BUFFER_BIT);
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+    glBegin(GL_TRIANGLES);
+        glColor3f(1.0f, 0.0f, 0.0f); glVertex2f(-0.5f, -0.5f);
+        glColor3f(0.0f, 1.0f, 0.0f); glVertex2f( 0.5f, -0.5f);
+        glColor3f(0.0f, 0.0f, 1.0f); glVertex2f( 0.0f,  0.5f);
+    glEnd();
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
+    glutSwapBuffers();
+}
+
+int main(int argc, char** argv)
+{
+    // --- test GLM ---
+    glm::vec3 v1(1.0f, 2.0f, 3.0f);
+    glm::vec3 v2(4.0f, 5.0f, 6.0f);
+    glm::vec3 v3 = glm::cross(v1, v2);
+    std::cout << "Cross product (GLM test): "
+              << v3.x << ", " << v3.y << ", " << v3.z << std::endl;
+
+    // --- init GLUT ---
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(640, 480);
+    glutCreateWindow("FreeGLUT + GLEW + GLM test");
+
+    // --- init GLEW ---
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
     {
-        glfwTerminate();
-        return -1;
+        std::cerr << "GLEW error: " << glewGetErrorString(err) << std::endl;
+        return 1;
     }
+    std::cout << "Using GLEW version: " << glewGetString(GLEW_VERSION) << std::endl;
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    // --- OpenGL info ---
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+    // set display callback
+    glutDisplayFunc(display);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+    // start event loop
+    glutMainLoop();
 
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
     return 0;
 }
+
